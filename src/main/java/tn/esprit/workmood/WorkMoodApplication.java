994e22dfb8c197be4b3bb6e95f85fc57e31280e6
PC <abdelhamid.atallah@esprit.tn>
2022-02-27 12:21:14 +0100

    initial commit

---
+++ src/main/java/tn/esprit/workmood/WorkMoodApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.workmood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkMoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkMoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/workmood/WorkMoodApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.workmood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkMoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
